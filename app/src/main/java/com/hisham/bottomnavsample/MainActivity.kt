28d08f1e6ccb8e739ca008fd5fdc14651ec6ecca
Hisham <hisham.dbd@gmail.com>
2023-06-02 01:06:18 +0530

    Initial commit

---
+++ app/src/main/java/com/hisham/bottomnavsample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hisham.bottomnavsample
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.hisham.bottomnavsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        val fab = binding.fab
+        fab.setOnClickListener {
+            navController.navigate(R.id.navigation_dashboard)
+        }
+    }
+}
